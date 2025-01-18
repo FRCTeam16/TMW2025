3247d0272c2f780af7afd541a8fed28ca6bbb591
AustinParah <parahaustin@gmail.com>
2025-01-17 21:14:47 -0600

    large changes on VisionAssist(untested)
    now we
       - filter non-reefTags
       - find x and y meters to tag ( robot centric )
       - can convert robot centric to field centric ( needs pigeon work though )

--- src/main/java/frc/robot/subsystems/vision/VisionAssist.java
+++ src/main/java/frc/robot/subsystems/vision/VisionAssist.java
@@ -4,51 +4,75 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.hci.SwerveSupplier;
+import frc.robot.util.PIDHelper;
 import frc.robot.util.VisionUtil;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.units.measure.LinearVelocity;
+import static edu.wpi.first.units.Units.MetersPerSecond;
 
 public class VisionAssist {
-    private double x = 0;
-    private double y = 0;
-    private double rotation = 0;
-    private boolean isRunning = false;
+    private double distance = 0;
     private VisionUtil limeLight = new VisionUtil("LimeLight");
+    double tagAngle;
+    double robotAngle;
+    private double robotCentricX = 0;
+    private double robotCentricY = 0;
+    private double fieldCentricX = 0;
+    private double fieldCentricY = 0;
 
-    double tagAngle;   //
-    double robotAngle; // pidgon for doing feild centric assist
-
+    private PIDHelper speedHelper = new PIDHelper("VisionAssist", true);
 
     private SwerveRequest.FieldCentric drive;
 
     public VisionAssist(SwerveRequest.FieldCentric drive) {
         this.drive = drive;
+
     }
 
     public SwerveRequest deferDrive(SwerveSupplier swerveSupplier){
         DriverStation.reportWarning("Defer Drive Reached", false);
-        return drive.withVelocityX(swerveSupplier.supplyX()) // Drive forward with negative Y (forward)
-                .withVelocityY(swerveSupplier.supplyY()) // Drive left with negative X (left)
+        return drive.withVelocityX(swerveSupplier.supplyX().plus(MetersPerSecond.of(fieldCentricX))) // Drive forward with negative Y (forward) + (distance * Math.cos(180-tagAngle))
+                .withVelocityY(swerveSupplier.supplyY().plus(MetersPerSecond.of(fieldCentricY))) // Drive left with negative X (left)
                 .withRotationalRate(swerveSupplier.supplyRotationalRate()); // Drive counterclockwise with negative X (left)
     }
 
-    public double getX(){
-        if(isRunning)
-            return x;
-        return 0;
+    public boolean isCoralTarget(int tagNumber){
+        if(tagNumber == 17 || tagNumber == 6)
+            return true;
+        if(tagNumber == 18 || tagNumber == 7)
+            return true;
+        if(tagNumber == 19 || tagNumber == 8)
+            return true;
+        if(tagNumber == 20 || tagNumber == 9)
+            return true;
+        if(tagNumber == 21 || tagNumber == 10)
+            return true;
+        if(tagNumber == 22 || tagNumber == 11)
+            return true;
+        return false;
     }
 
-    public double getY(){
-        if(isRunning)
-            return y;
-        return 0;
+    public void periodic(){
+        
+        if(limeLight.hasTarget() && isCoralTarget(limeLight.getTagID())){
+            distance = limeLight.getDistanceToTag(0.017, 0.018, 0); // TODO: use not unreliable numbers
+            tagAngle = limeLight.calculateAngleToTag();
+
+            // Polar Coordinates to cartesian coordinates
+            robotCentricX = distance * Math.cos(180 - tagAngle);
+            robotCentricY = distance * Math.cos(180 - tagAngle);
+
+            // rotate coordinates by angle of pigeon
+            fieldCentricX = robotCentricX * Math.cos(robotAngle) - robotCentricY * Math.sin(robotAngle);
+            fieldCentricY = robotCentricX * Math.cos(robotAngle) - robotCentricY * Math.cos(robotAngle);
+        }
+        else{
+            fieldCentricX = 0;
+            fieldCentricY = 0;
+        }
     }
 
-    public double getRotation(){
-        if(isRunning)
-            return rotation;
-        return 0;
-    }
 
-    
-    
+
+
 }
