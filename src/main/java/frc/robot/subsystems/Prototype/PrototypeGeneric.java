ad570d6a16854fd7f309a09975a8dd5ec21fe528
AustinParah <parahaustin@gmail.com>
2025-01-23 10:45:51 -0600

    Permenant fix on GenericComposedPrototype
    Bug caused by too many subtypes on the composed config lambda ( Consumer<ArrayList<PrototypeGeneric>> )
    
    added a config constructor for each individual component, alongside the default one

--- src/main/java/frc/robot/subsystems/Prototype/PrototypeGeneric.java
+++ src/main/java/frc/robot/subsystems/Prototype/PrototypeGeneric.java
@@ -1,9 +1,5 @@
 package frc.robot.subsystems.Prototype;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import edu.wpi.first.wpilibj2.command.Command;
 
 public interface PrototypeGeneric {
--- src/main/java/frc/robot/subsystems/Prototype/PrototypeGenericMotor.java
+++ src/main/java/frc/robot/subsystems/Prototype/PrototypeGenericMotor.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.Prototype;
 
+import java.util.function.Consumer;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.NeutralOut;
@@ -29,7 +31,12 @@ public class PrototypeGenericMotor implements Lifecycle, Subsystem, PrototypeGen
     private final DutyCycleOut backward = new DutyCycleOut(0);
     private String ElasticName;
 
+    // Overloaded constructor for default configs
     public PrototypeGenericMotor(String ElasticName, int defaultId){
+        this(ElasticName, defaultId, (temp)->{});
+    }
+
+    public PrototypeGenericMotor(String ElasticName, int defaultId, Consumer<PrototypeGenericMotor> config){
         this.ElasticName = ElasticName;
         SmartDashboard.setDefaultNumber(ElasticName + "/motorID", defaultId);
         motor = new TalonFX((int)SmartDashboard.getNumber(ElasticName + "/motorID", defaultId));
@@ -39,8 +46,11 @@ public class PrototypeGenericMotor implements Lifecycle, Subsystem, PrototypeGen
 
         SmartDashboard.setDefaultNumber(ElasticName + "/runForward", -0);
         SmartDashboard.setDefaultNumber(ElasticName + "/runBackward", 0);
+        
+        config.accept(this);
     }
 
+    
     public Command runForward(){
         return this.runOnce(() -> {
         motor.setControl(forward.withOutput(SmartDashboard.getNumber(ElasticName+"/runForward", 0)));
@@ -65,7 +75,7 @@ public class PrototypeGenericMotor implements Lifecycle, Subsystem, PrototypeGen
         });
     }
 
-    public Command setDirection(PrototypeGenericMotor.direction dir){
+    public Command setDirection(PrototypeGenericMotor.direction dir){ // generally usedfull for config lambdas
         return this.runOnce(() -> {
             SmartDashboard.setDefaultNumber(ElasticName + "/runForward", SmartDashboard.getNumber(ElasticName + "/runForward", 0) * dir.value);
             SmartDashboard.setDefaultNumber(ElasticName + "/runBackward", SmartDashboard.getNumber(ElasticName + "/runBackward", 0) * -dir.value);
