0ac2d1fd2ca8f69c63c13a43747d9aa6f5155239
Jason Smith <devjason@gmail.com>
2025-03-28 19:49:16 -0500

    New latch servo

--- src/main/java/frc/robot/subsystems/Intake/FunnelSubsystem.java
+++ src/main/java/frc/robot/subsystems/Intake/FunnelSubsystem.java
@@ -2,7 +2,8 @@ package frc.robot.subsystems.Intake;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.util.sendable.SendableBuilder;
-import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -10,20 +11,22 @@ import frc.robot.subsystems.Lifecycle;
 import frc.robot.util.BSLogger;
 
 public class FunnelSubsystem extends SubsystemBase implements Lifecycle {
-    private PWM funnelLatch = new PWM(0);
+    private final Servo funnelLatch = new Servo(0);
     private double setpoint = 0.0;
     private boolean latchOpened = true; // assume we start true
+
+    public FunnelSubsystem() {
+    }
 
     @Override
     public void initSendable(SendableBuilder builder) {
         super.initSendable(builder);
         builder.setSmartDashboardType("FunnelSubsystem");
-        builder.addBooleanProperty("In Position", this::inPosition, null);
+        builder.addBooleanProperty("Latch Opened", () -> latchOpened, null);
 
         if (Constants.DebugSendables.Funnel) {
-            builder.addDoubleProperty("Position", funnelLatch::getPosition, null);;
-            builder.addDoubleProperty("Setpoint", () -> setpoint, this::setSetpoint);
-            builder.addBooleanProperty("Latch Opened", () -> latchOpened, null);
+            builder.addDoubleProperty("Position", funnelLatch::getPosition, null);
+            builder.addBooleanProperty("In Position", this::inPosition, null);
         }
     }
 
@@ -46,13 +49,12 @@ public class FunnelSubsystem extends SubsystemBase implements Lifecycle {
     public void closeLatch() {
         BSLogger.log("Funnel", "closeLatch");
         latchOpened = false;
-        this.setSetpoint(0.0);
+        this.setSetpoint(0.1);
     }
 
     public Command openLatchCommand() {
         return this.runOnce(this::openLatch);
     }
-
 
     public Command closeLatchCommand() {
         return this.runOnce(this::closeLatch);
